ee7274e4f4041bd8809f48bfe9683f8de90e602e
Jeremy Adams <jeremy@dagger.io>
2022-11-20 08:14:42 -0800

    split Dagger pipeline and GoodData script
    Signed-off-by: Jeremy Adams <jeremy@dagger.io>

--- pipeline.py
+++ pipeline.py
@@ -1,46 +1,34 @@
-"""
-Execute a command
-"""
 
 import sys
 import anyio
 import dagger
-import gooddata_sdk
-from gooddata_sdk.catalog.workspace.entity_model.workspace import CatalogWorkspace
 import os
 
+host = os.getenv('GOODDATA_HOST')
+token = os.getenv('GOODDATA_TOKEN')
+staging_workspace_id = os.getenv('GOODDATA_STAGING_WORKSPACE_ID')
+production_workspace_id = os.getenv('GOODDATA_PRODUCTION_WORKSPACE_ID')
 
 async def pipeline():
     config = dagger.Config(log_output=sys.stderr)
+    async with dagger.Connection(config) as client:
 
-    async with dagger.Connection(config):
-        host = os.getenv('GOODDATA_HOST')
-        token = os.getenv('GOODDATA_TOKEN')
-        staging_workspace_id = os.getenv('GOODDATA_STAGING_WORKSPACE_ID')
-        production_workspace_id = os.getenv('GOODDATA_PRODUCTION_WORKSPACE_ID')
+        src_id = await client.host().directory(".", include=["gd.py"]).id()
 
-        sdk = gooddata_sdk.GoodDataSdk.create(host, token)
-
-        sdk.catalog_workspace.create_or_update(
-            CatalogWorkspace(production_workspace_id, production_workspace_id)
+        ctr = ( 
+             client.container()
+            .from_("python:alpine")
+            .exec(["pip", "install", "gooddata-sdk"])
+            .with_mounted_directory("/src", src_id)
+            .with_workdir("/src")
+			.with_env_variable('GOODDATA_HOST', host)
+			.with_env_variable('GOODDATA_TOKEN', token)
+			.with_env_variable('GOODDATA_STAGING_WORKSPACE_ID', staging_workspace_id)
+			.with_env_variable('GOODDATA_PRODUCTION_WORKSPACE_ID', production_workspace_id)
+            .exec(["python", "gd.py"])
         )
-
-        declarative_ldm = sdk.catalog_workspace_content.get_declarative_ldm(staging_workspace_id)
-        declarative_analytics_model = sdk.catalog_workspace_content.get_declarative_analytics_model(
-            staging_workspace_id
-        )
-
-        sdk.catalog_workspace_content.put_declarative_ldm(
-            production_workspace_id,
-            declarative_ldm
-        )
-        sdk.catalog_workspace_content.put_declarative_analytics_model(
-            production_workspace_id,
-            declarative_analytics_model
-        )
-
-        print("done")
-
+        result = await ctr.stdout().contents()
+        print(result)
 
 if __name__ == "__main__":
     anyio.run(pipeline)
